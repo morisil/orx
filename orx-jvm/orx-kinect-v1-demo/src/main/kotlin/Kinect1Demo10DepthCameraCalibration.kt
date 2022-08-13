dc7c75a9ca439b7388d3edd46ccb522307aa187b
Kazik Pogoda <morisil@xemantic.com>
2022-08-13 13:35:51 +0200

    the first complete version of Kinect1Demo10DepthCameraCalibration.kt

--- orx-jvm/orx-kinect-v1-demo/src/main/kotlin/Kinect1Demo10DepthCameraCalibration.kt
+++ orx-jvm/orx-kinect-v1-demo/src/main/kotlin/Kinect1Demo10DepthCameraCalibration.kt
@@ -9,12 +9,10 @@ import org.openrndr.extra.depth.camera.DepthMeasurement
 import org.openrndr.extra.depth.camera.calibrator.DepthCameraCalibrator
 import org.openrndr.extra.gui.GUI
 import org.openrndr.extra.kinect.v1.Kinect1
+import org.openrndr.math.Vector2
 
 /**
- * It is possible to rewrite raw kinect value interpretation completely
- * while keeping all the performance characteristics.
- *
- * Note: when depth measurement is set to RAW, the flip options does not apply.
+ * How to use [DepthCameraCalibrator] with [Kinect1]?
  */
 fun main() = application {
     configure {
@@ -24,36 +22,73 @@ fun main() = application {
         val kinect = extend(Kinect1())
         val device = kinect.openDevice()
         val camera = device.depthCamera
-        camera.depthMeasurement = DepthMeasurement.RAW
-        val calibrator = extend(DepthCameraCalibrator(camera))
-        val outputBuffer = colorBuffer(camera.resolution.x, camera.resolution.y)
-        val filter = Filter(filterShaderFromCode("""
-            layout(origin_upper_left) in vec4 gl_FragCoord;            
-            uniform usampler2D  tex0;             // kinect raw
-            out     vec4        o_color;
-
-            void main() {
-                ivec2 uv = ivec2(gl_FragCoord);
-                uint uintDepth = texelFetch(tex0, uv, 0).r;
-                float depth = float(uintDepth) / 2047.;
-                o_color = vec4(vec3(depth), 1.);
-            }
-            """.trimIndent(),
-            "raw filter")
+        camera.depthMeasurement = DepthMeasurement.METERS
+        val outputBuffer = colorBuffer(
+            camera.resolution.x,
+            camera.resolution.y
         )
+        val spaceRangeExtractor = SpaceRangeExtractor()
+        spaceRangeExtractor.resolution = Vector2(width.toDouble(), height.toDouble())
+        val calibrator = extend(DepthCameraCalibrator(camera))
+        calibrator.onCalibrationChange { camera, calibration ->
+            spaceRangeExtractor.minDepth = calibration.minDepth
+            spaceRangeExtractor.maxDepth = calibration.maxDepth
+            camera.flipH = calibration.flipH
+            camera.flipV = calibration.flipV
+        }
         camera.onFrameReceived { frame ->
-            filter.apply(frame, outputBuffer)
+            spaceRangeExtractor.apply(frame, outputBuffer)
         }
         device.depthCamera.enabled = true
+        val gui = GUI()
+//        extend() {
+//            visible = false
+//            compartmentsCollapsedByDefault = false
+//        }
+        calibrator.addControlsTo(gui)
         extend {
-            drawer.image(outputBuffer)
+            val calibration = calibrator.getCalibration(camera)
+            drawer.image(
+                colorBuffer = outputBuffer,
+                position = calibration.position,
+                width = calibration.width,
+                height = calibration.height
+            )
         }
-        val gui = extend(GUI())
+        extend(gui)
 
         program.keyboard.keyDown.listen {
             if (it.name == "k") {
                 calibrator.enabled = !calibrator.enabled
             }
         }
+
     }
+
+}
+
+/**
+ * A visual effect applied to kinect data in this demonstration.
+ * Everything is black, except for the white pixels within the range
+ * of 2 virtual walls positioned at [minDepth] at front of the
+ * viewer and [maxDepth] behind the viewer.
+ */
+class SpaceRangeExtractor : Filter(filterShaderFromCode("""
+    uniform usampler2D  tex0;             // kinect raw
+    uniform vec2        resolution;
+    uniform float       minDepth;
+    uniform float       maxDepth;
+    out     vec4        o_color;
+    void main() {
+        vec2 uv = gl_FragCoord.xy / resolution;
+        float depth = texture(tex0, uv).r;
+        float luma = ((depth >= minDepth) && (depth <= maxDepth)) ? 1.0 : 0.0;
+        o_color = vec4(vec3(luma), 1.0);
+    }
+    """.trimIndent(),
+    "space range extractor"
+)) {
+    var resolution: Vector2 by parameters
+    var minDepth: Double by parameters
+    var maxDepth: Double by parameters
 }
