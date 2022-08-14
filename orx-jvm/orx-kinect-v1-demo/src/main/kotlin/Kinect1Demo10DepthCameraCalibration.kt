7e6f2d0553b6ef8d925e9a5f469e0dc8395b7a3d
Kazik Pogoda <morisil@xemantic.com>
2022-08-14 21:32:12 +0200

    refined Kinect1Demo10DepthCameraCalibration.kt

--- orx-jvm/orx-kinect-v1-demo/src/main/kotlin/Kinect1Demo10DepthCameraCalibration.kt
+++ orx-jvm/orx-kinect-v1-demo/src/main/kotlin/Kinect1Demo10DepthCameraCalibration.kt
@@ -7,9 +7,9 @@ import org.openrndr.draw.colorBuffer
 import org.openrndr.draw.filterShaderFromCode
 import org.openrndr.extra.depth.camera.DepthMeasurement
 import org.openrndr.extra.depth.camera.calibrator.DepthCameraCalibrator
+import org.openrndr.extra.depth.camera.calibrator.isolatedWithCalibration
 import org.openrndr.extra.gui.GUI
 import org.openrndr.extra.kinect.v1.Kinect1
-import org.openrndr.math.Vector2
 
 /**
  * How to use [DepthCameraCalibrator] with [Kinect1]?
@@ -19,44 +19,69 @@ fun main() = application {
         fullscreen = Fullscreen.CURRENT_DISPLAY_MODE
     }
     program {
+
         val kinect = extend(Kinect1())
         val device = kinect.openDevice()
         val camera = device.depthCamera
+        // depth measurement in meters is required by the calibrator
         camera.depthMeasurement = DepthMeasurement.METERS
+        val kinectResolution = camera.resolution
+
         val outputBuffer = colorBuffer(
-            camera.resolution.x,
-            camera.resolution.y
+            kinectResolution.x,
+            kinectResolution.y
         )
+
+        // simple visual effect applied to kinect data
         val spaceRangeExtractor = SpaceRangeExtractor()
-        spaceRangeExtractor.resolution = Vector2(width.toDouble(), height.toDouble())
-        val calibrator = extend(DepthCameraCalibrator(camera))
-        calibrator.onCalibrationChange { camera, calibration ->
-            spaceRangeExtractor.minDepth = calibration.minDepth
-            spaceRangeExtractor.maxDepth = calibration.maxDepth
-            camera.flipH = calibration.flipH
-            camera.flipV = calibration.flipV
-        }
         camera.onFrameReceived { frame ->
             spaceRangeExtractor.apply(frame, outputBuffer)
         }
-        device.depthCamera.enabled = true
+        val calibrator = DepthCameraCalibrator(this, camera)
+
         val gui = GUI()
-//        extend() {
-//            visible = false
-//            compartmentsCollapsedByDefault = false
-//        }
         calibrator.addControlsTo(gui)
-        extend {
-            val calibration = calibrator.getCalibration(camera)
-            drawer.image(
-                colorBuffer = outputBuffer,
-                position = calibration.position,
-                width = calibration.width,
-                height = calibration.height
-            )
-        }
+
+        /*
+         Note: remember that extend(gui) has to be called after all the parameter
+         controls are added.
+
+         Also extensions are rendered in reverse order, if we start with gui,
+         it will not be covered by calibrator view when calibrator is enabled
+         */
         extend(gui)
 
+        /*
+         if it's an interactive installation, probably we don't want to
+         show GUI on startup. It can be shown by pressing F11.
+         */
+        gui.visible = false
+
+        /*
+         Registering this callback here after gui will prevent it from
+         being triggered multiple times when GUI parameters are restored
+         on startup.
+         */
+        calibrator.onCalibrationChange { calibration ->
+            spaceRangeExtractor.minDepth = calibration.minDepth
+            spaceRangeExtractor.maxDepth = calibration.maxDepth
+        }
+        extend(calibrator)
+        camera.enabled = true
+
+        extend {
+            val calibration = calibrator.getCalibration(camera)
+            drawer.isolatedWithCalibration(calibration) {
+                image(
+                    colorBuffer = outputBuffer,
+                    position = calibration.position,
+                    width = calibration.width,
+                    height = calibration.height
+                )
+            }
+        }
+
+        // switching calibrator view on and off with keyboard
         program.keyboard.keyDown.listen {
             if (it.name == "k") {
                 calibrator.enabled = !calibrator.enabled
@@ -74,21 +99,19 @@ fun main() = application {
  * viewer and [maxDepth] behind the viewer.
  */
 class SpaceRangeExtractor : Filter(filterShaderFromCode("""
-    uniform usampler2D  tex0;             // kinect raw
-    uniform vec2        resolution;
+    uniform sampler2D   tex0;             // kinect raw
     uniform float       minDepth;
     uniform float       maxDepth;
     out     vec4        o_color;
     void main() {
-        vec2 uv = gl_FragCoord.xy / resolution;
-        float depth = texture(tex0, uv).r;
+        ivec2 uv = ivec2(gl_FragCoord.xy);
+        float depth = texelFetch(tex0, uv, 0).r;
         float luma = ((depth >= minDepth) && (depth <= maxDepth)) ? 1.0 : 0.0;
         o_color = vec4(vec3(luma), 1.0);
     }
     """.trimIndent(),
     "space range extractor"
 )) {
-    var resolution: Vector2 by parameters
     var minDepth: Double by parameters
     var maxDepth: Double by parameters
 }
